8105ff6c24bbc13bcc71bf9c5c41220f261240d3
George John <george@renfos.com>
2016-02-24 13:10:47 +0530

    bug fix

--- resources/views/admin/task/drop.blade.php
+++ resources/views/admin/task/drop.blade.php
@@ -58,6 +58,6 @@
                     </ul><div class="slimScrollBar" style="width: 3px; position: absolute; top: 0px; opacity: 0.4; display: block; border-radius: 7px; z-index: 99; right: 1px; background: rgb(0, 0, 0);"></div><div class="slimScrollRail" style="width: 3px; height: 100%; position: absolute; top: 0px; display: none; border-radius: 7px; opacity: 0.2; z-index: 90; right: 1px; background: rgb(51, 51, 51);"></div></div>
                   </li>
                   <li class="footer">
-                    <a href="{!! URL::to('admin/task/task') !!}">View all tasks</a>
+                    <a href="{!! trans_url('admin/task/task') !!}">View all tasks</a>
                   </li>
                 </ul>
